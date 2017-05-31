8a167975f9b2c2bc19cf09ffeba8b93aa0bbb3aa
milindmrao <milindmrao@gmail.com>
2017-05-31 16:51:18 -0700

    syncing the preprocessor

--- code/preprocess.py
+++ code/preprocess.py
@@ -16,6 +16,11 @@ import pickle
 """ creating a file with a sentence in a new line and sorting based on sentence
 length for the reuters and qasys datasets """
 
+UNK_ID = 0
+PAD_ID = 1
+START_ID = 2
+END_ID = 3
+
 def combine_input(file_name,params = {}):
     """ creates file from Reuters dataset. Prints
     Input:
@@ -50,11 +55,27 @@ def combine_input(file_name,params = {}):
     output_file_name = params.get('output_file_name',file_name)
     with open(os.path.join(parent_dir,'data',output_file_name+'_reuters.pickle'),'wb') as fop:
         pickle.dump(sentences,fop)
+    return sentences
+
+def process_sentence_eur():
+    parent_dir = os.path.split(os.getcwd())[0]
+    path_to_eur = os.path.join(parent_dir,'data','corpora','europarl-v7.en','europarl-v7.en')
+    sentences = []
+    word_token = nltk.tokenize.WordPunctTokenizer()
+    with open(path_to_eur,'r',encoding='utf-8') as fop:
+        for line in tqdm(fop.readlines()):
+            _len_line = len(word_token.tokenize(line))
+            if _len_line>=4 and _len_line<=40:
+                sentences +=[line]
+            
+    target_path = os.path.join(parent_dir,'data','training_euro.pickle')
+    pickle.dump(sentences,open(target_path,'wb'))
     
 def word_to_vec(length=50000,dim=50):
     """Aim of this is to create the vocabulary and the embeddings"""
     parent_dir,_ = os.path.split(os.getcwd())
-    words = ['<unk>','<pad>','<start>','<end>']
+    words_special = [('<unk>',UNK_ID),('<pad>',PAD_ID),('<start>',START_ID),('<end>',END_ID)]
+    words = []
     embedding = [list(np.random.randn(dim)) for x in range(len(words))]
     with open(os.path.join(parent_dir,'data','glove.6b.'+str(dim)+'d.txt'),'r',encoding="utf8") as fop:
         
@@ -70,10 +91,10 @@ def word_to_vec(length=50000,dim=50):
             line = fop.readline().split()
             words += [line[0]]
             embedding += [list(map(float,line[1:]))]
-    word2num = dict(zip(words,np.arange(len(words))))
-    num2word = dict(zip(np.arange(len(words)),words))
+    word2num = dict(words_special+zip(words,np.arange(len(words))))
+    num2word = dict([(n,w) for w,n in word2num.items()])
     embedding = np.array(embedding)
-    with open(os.path.join(parent_dir,'data',str(dim)+'_embed.pickle'),'wb') as fop:
+    with open(os.path.join(parent_dir,'data',str(dim)+'_embed_large.pickle'),'wb') as fop:
         pickle.dump(embedding,fop)
         
     with open(os.path.join(parent_dir,'data','w2n_n2w.pickle'),'wb') as fop:
@@ -84,9 +105,11 @@ def test_w2n():
     with open(os.path.join(parent_dir,'data','w2n_n2w.pickle'),'rb') as fop:
         [w2n,n2w] = pickle.load(fop)
     return [w2n,n2w]
-        
+
+
 if __name__ == "__main__":
-    combine_input('training',{'output_file_name':'training_rest','min_length':4,'max_length':50})
+    #combine_input('test',{'output_file_name':'test_rest','min_length':4,'max_length':50})
     #combine_input('test')
-    #word_to_vec()
+    #word_to_vec(200000,100)
+    sentences = process_sentence_eur()
     pass
